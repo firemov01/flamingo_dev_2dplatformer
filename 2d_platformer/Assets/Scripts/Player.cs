3ff539ecfa0a723fd0633b77b672e9ef5957e291
Ionut Constantin <Constantin.Gr.Ionut@student.utcluj.ro>
2023-04-23 22:36:51 +0300

    CI/II-4: Jump Mechanics

--- 2d_platformer/Assets/Scripts/Player.cs
+++ 2d_platformer/Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player : MonoBehaviour
 {
     Rigidbody2D rb;
@@ -9,16 +10,19 @@ public class Player : MonoBehaviour
     public float jumpForce = 5f;
     float horizontalValue;
     float verticalValue;
+    public GroundCheck groundCheck;
+    public CapsuleCollider2D capsuleCollider;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -31,6 +35,7 @@ public class Player : MonoBehaviour
     void FixedUpdate()
     {
         Move(horizontalValue);
+        Jump();
     }
 
     void Move(float direction)
@@ -40,6 +45,9 @@ public class Player : MonoBehaviour
 
     void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (verticalValue > 0 && groundCheck.isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce * 40 * Time.deltaTime);
+        }
     }
 }
